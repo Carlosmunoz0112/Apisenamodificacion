06ce03d00fc4626fb45bb6b3c95d44053ac5d08e
carlosmunoz0112 <carlosmunoz0112@gmail.com>
2023-09-13 08:31:27 -0500

    apipruebasexito

--- models/factura.js
+++ models/factura.js
@@ -1,37 +1,29 @@
 const { Schema, model } = require('mongoose');
 
-const productoSchema = new Schema({
-    nombre: {
-        type: String,
-        required: [true, 'El nombre del producto es obligatorio']
-    },
-    precio: {
-        type: Number,
-        required: [true, 'El precio del producto es obligatorio']
-    },
-    cantidad: {
-        type: Number,
-        required: [true, 'La cantidad del producto es obligatoria']
-    }
-});
+
 
 const facturaSchema = new Schema({
     nombreFactura: {
         type: String,
         required: [true, 'El nombre de la factura es obligatorio']
     },
+    producto: {
+        type: String,
+        required: [true, 'el nombre del producto es obligatorio']},
+precio: {
+    type: Number,
+    required: [true, 'El precio es obligatorio obligatorio']
+},
+cantidad: {
+    type: Number,
+    required: [true, 'la cantidad es obligatoria obligatorio']
+},
+
     fechaVenta: {
-        type: Date,
-        required: [true, 'La fecha de venta es obligatoria'],
-        validate: {
-            validator: function (fechaVenta) {
-                const fecha = new Date();
-                return fechaVenta < fecha;
-            },
-            message: 'La fecha de venta es mayor a la fecha actual'
-        }
+        type: String,
+required: [true, 'la fecha es obligatoria']
+        
     },
-    productos: [productoSchema], // Arreglo de productos
     estado: {
         type: Boolean,
         default: true,
@@ -48,13 +40,5 @@ const facturaSchema = new Schema({
 module.exports = model('Factura', facturaSchema);
 
 
-// Antes de guardar, calcula el valor del campo 'total' multiplicando 'precio' por 'cantidad'
-facturaSchema.pre('save', function (next) {
-    const totalProductos = this.productos.reduce((total, producto) => {
-        return total + producto.precio * producto.cantidad;
-    }, 0);
-    this.total = totalProductos;
-    next();
-});
 
 module.exports = model('factura', facturaSchema);
